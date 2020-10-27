c91bdebaf779a848902532ac67805dd196c69ce0
Ronaldo Aquino <ronaldoaquinodev@gmail.com>
2020-10-27 13:53:17 -0200

    Teste no formulário com fireEvent

--- src/__tests__/Formulario.js
+++ src/__tests__/Formulario.js
@@ -1,17 +1,19 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import Formulario from "../components/Formulario";
 import "@testing-library/jest-dom/extend-expect";
+
+const crearCita = jest.fn();
 
 test("<Formulario / > Carregar o formulário e revisar se está renderizando corretamente", () => {
     // const wrapper = render(<Formulario />);
     // wrapper.debug();
 
-    render(<Formulario />);
+    render(<Formulario crearCita={crearCita} />);
     expect(screen.getByText("Crear Cita")).toBeInTheDocument();
 
     // Header
-    const titulo  = screen.getByTestId("titulo")
+    const titulo = screen.getByTestId("titulo");
     expect(titulo.tagName).toBe("H2");
     expect(titulo.tagName).not.toBe("H1");
     expect(titulo.textContent).toBe("Crear Cita");
@@ -21,5 +23,20 @@ test("<Formulario / > Carregar o formulário e revisar se está renderizando cor
     expect(btnSubmit.tagName).toBe("BUTTON");
     expect(btnSubmit.textContent).toBe("Agregar Cita");
     expect(btnSubmit.textContent).not.toBe("Agregar Nueva Cita");
+});
 
+test("<Formulario /> Validação do formulário", () => {
+    render(<Formulario crearCita={crearCita} />);
+
+    // Clique no botão de submit
+    const btnSubmit = screen.getByTestId("btn-submit");
+    fireEvent.click(btnSubmit);
+
+    // Revisar a validação
+    expect(screen.getByTestId("alerta")).toBeInTheDocument();
+    expect(screen.getByTestId("alerta").textContent).toBe(
+        "Todos los campos son obligatorios"
+    );
+    expect(screen.getByTestId("alerta").tagName).toBe("P");
+    expect(screen.getByTestId("alerta").tagName).not.toBe("BUTTON");
 });
